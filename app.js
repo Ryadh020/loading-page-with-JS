8f8a3be2042924081ff6b344c0c85b81accd7a0e
riyadh <riyadh02220@gmail.com>
2019-04-10 13:52:09 +0200

    aroow are moving up/down

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-// get access to the arrows:
- /*let arrowL = document.querySelector('#Left');
- arrowL.innerHTML = "&#xf103;"*/
+// CONSTRUCTOR FUNCTION :
 
- function Arrow(id) {
-     this.fill = function(id) {
-        const type = document.querySelector(`#${id}`);
-        type.innerHTML = "&#xf103;"
+function Icons() {
+     this.fillArrow = function(id) {
+               // repeat the transition every 1s:
+        setInterval(function(){
+               // get acces to the wanted icon and set the initial position:
+            let arr = document.querySelector(`${id}`);
+            arr.innerHTML = "&#xf103;"
+            arr.style.paddingTop = "9px";
+               // set the following position :
+            setTimeout(function() {
+               arr.style.paddingTop = "14px";
+            }, 500)
+        }, 1000)  
      }
 } 
-let arrowR = document.querySelector('#right');
- arrowR =  new Arrow(arrowR,"right");
-arrowR.fill;
+
+const arrowR =  new Icons();
+arrowR.fillArrow("#right");
+
+
+const arrowL = new Icons();
+arrowL.fillArrow("#Left");
+
+
+
 /*
  let arrowR = document.querySelector('#right');
  arrowR.innerHTML = "&#xf103;"*/
+
+
+ /*let arrowR = document.querySelector('#right');*/
  
+ // get access to the arrows:
+ /*let arrowL = document.querySelector('#Left');
+ arrowL.innerHTML = "&#xf103;"*/
